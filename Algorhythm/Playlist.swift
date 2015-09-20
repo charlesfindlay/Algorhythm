dde00ce1df6a4e0fb5d885495e5593ee76b862ba
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-20 14:59:41 -0400

    Created Playlist struct and added properties

---
+++ Algorhythm/Playlist.swift
@@ -0,0 +1,21 @@
+//
+//  Playlist.swift
+//  Algorhythm
+//
+//  Created by Student on 9/20/15.
+//  Copyright (c) 2015 Charles Findlay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
